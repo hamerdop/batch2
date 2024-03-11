3058e15d96be99709f9d5d958408740541b4c6b6
hamerdop <162981056+hamerdop@users.noreply.github.com>
2024-03-11 16:17:58 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,47 @@
+bargained characters
+  controls and data structures....>ABC language
+
+  strings amd its formats.....>from c
+
+  regular expressions.....>pearl
+
+  exception gandling....>module 3
+
+  magic method from...>fortron
+   
+
+
+Flavours of Python
+A
+Cpython - Written in C, Most common Implementation of python.
+A
+Jython - Java implementation of Python.
+A
+IronPython - implementation of the Python programming language which is tightly integrated with .NET.
+A
+Brython - Browser Python, runs in browsers
+A
+RubyPython - Bridge between Python and Ruby Interpreters
+MicroPython - Runs in Micro Controllers
+
+#---> print(object, sep, end, file, flush) #print has 5 parameters
+#object-->the value to be printed is object
+#sep--> seperator
+# Eg
+#a8
+#b9
+# C = 89
+1
+#print(a, b, c, sep='$')
+# end --> which is used to add the string at the end of the value # # It will not keep the command to stay in same line
+# Eg:
+# print(str(a), b, end='%%')
+# print(str(c))
+
+#! Rules to dedfine variables
+#1.) Variable have to start with only alphabets, underscores
+#2.) The char following the 1st char can be alphabets, numbers, underscores
+#3.) No spaces are allowed
+# 4.) No special characters like !@#$%^&*()+
+#5.) case sensitive
+#6.) Keywords are not allowed as variables
